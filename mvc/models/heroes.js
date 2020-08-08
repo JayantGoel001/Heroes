cf8e2848dc951f97c74f7481670d38e08c4670a5
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-08-09 02:45:12 +0530

    Heroes Project

--- mvc/models/heroes.js
+++ mvc/models/heroes.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
-const heroSchema = new mongoose.Schema({
-    name:String
+const statSchema = new mongoose.Schema({
+    strength :     {type : Number , default : 30, min:0, max:100},
+    perception :   {type : Number , default : 30, min:0, max:100},
+    endurance :    {type : Number , default : 30, min:0, max:100},
+    charisma :     {type : Number , default : 30, min:0, max:100},
+    intelligence : {type : Number , default : 30, min:0, max:100},
+    agility :      {type : Number , default : 30, min:0, max:100},
+    luck :         {type : Number , default : 30, min:0, max:100},
 });
 
+const heroSchema = new mongoose.Schema({
+    name : { type : String, required : true, unique : true},
+    description : {type : String, required : true},
+    origin : {type : String, default : "Unknown"},
+    stats : {type : statSchema, required : true}
+});
 
+const squadSchema = new mongoose.Schema({
+    name : { type:String , required : true , unique : true}
+});
 
 mongoose.model('Hero',heroSchema);
