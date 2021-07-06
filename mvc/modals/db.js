2ac738f159edeaa1cf9c787d6bb96e2b458894a3
JayantGoel001 <jgoel92@gmail.com>
2021-07-06 12:36:35 +0530

    Initial Commit

--- mvc/modals/db.js
+++ mvc/modals/db.js
@@ -4,4 +4,27 @@ const uri = "mongodb://localhost/Heroes";
 
 mongoose.connect(uri,{useNewUrlParser : true, useUnifiedTopology :true}).then( _ =>{
     console.log(`Mongoose Connected ${uri}`);
+}).catch(err =>{
+    console.log(`Error : ${err}`);
+});
+const shutdown = (msg, callback)=>{
+    mongoose.connection.close(()=>{
+        console.log(`Mongoose Disconnected ${msg}`);
+        callback();
+    });
+}
+process.once('SIGUSR2',()=>{
+    shutdown('nodemon restart',()=>{
+        process.kill(process.pid,'SIGUSR2');
+    });
+});
+process.on('SIGINT',()=>{
+    shutdown('app termination',()=>{
+        process.exit(0);
+    });
+});
+process.on('SIGTERM',()=>{
+    shutdown('Heroku app shutdown',()=>{
+        process.exit(0);
+    });
 });
