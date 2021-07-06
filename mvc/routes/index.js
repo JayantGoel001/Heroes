e97c376c2d07ef92032efd3d19b1806bb0360b23
JayantGoel001 <jgoel92@gmail.com>
2021-07-06 12:01:54 +0530

    Initial Commit

---
+++ mvc/routes/index.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
